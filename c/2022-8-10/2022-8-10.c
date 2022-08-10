4f1810bac3897509c0fbb0faf250da90c3c817df
qq8351176 <1045882688@qq.com>
2022-08-10 22:17:16 +0800

    c

--- c/2022-8-10/2022-8-10.c
+++ c/2022-8-10/2022-8-10.c
@@ -1,40 +1,56 @@
 ﻿#define _CRT_SECURE_NO_WARNINGS 0
-#include<stdio.h>
-int result()
-{
+#include <stdio.h>
+int main() {
     int n;
-    int num;
+    int i = 0;
+    int arr[50];
+    int del;
     scanf("%d", &n);
-    for (int i = 0;i < n; i++)
-    {
-        for (int j = 0;j < n; j++)
-        {
-            if (i > j)
-            {
-                scanf("%d", &num);
-                if (num)
-                {
-                    return 0;
-                }
-            }
-            else
-            {
-                scanf("%d", &num);
-            }
-        }
+    for (i = 0;i < n; i++) {
+        scanf("%d", arr[i]);
     }
-    return 1;
+    scanf("%d", &del);
+    for (i = 0; i < n; i++) {
+        if (arr[i] != del)
+            printf("%d ", arr[i]);
+    }
 }
-int main()
-{
-    if (result())
-    {
-        printf("YES\n");
-    }
-    else
-    {
-        printf("NO\n");
-    }
-    return 0;
-
-}
+//#include<stdio.h>
+//int result()
+//{
+//    int n;
+//    int num;
+//    scanf("%d", &n);
+//    for (int i = 0;i < n; i++)
+//    {
+//        for (int j = 0;j < n; j++)
+//        {
+//            if (i > j)
+//            {
+//                scanf("%d", &num);
+//                if (num)
+//                {
+//                    return 0;
+//                }
+//            }
+//            else
+//            {
+//                scanf("%d", &num);
+//            }
+//        }
+//    }
+//    return 1;
+//}
+//int main()
+//{
+//    if (result())
+//    {
+//        printf("YES\n");
+//    }
+//    else
+//    {
+//        printf("NO\n");
+//    }
+//    return 0;
+//
+//}
