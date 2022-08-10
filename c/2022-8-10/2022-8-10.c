59d988935bba5966f184afe1e0de7705c04fa50d
qq8351176 <1045882688@qq.com>
2022-08-10 20:54:19 +0800

    clearing

---
+++ c/2022-8-10/2022-8-10.c
@@ -0,0 +1,40 @@
+﻿#define _CRT_SECURE_NO_WARNINGS 0
+#include<stdio.h>
+int result()
+{
+    int n;
+    int num;
+    scanf("%d", &n);
+    for (int i = 0;i < n; i++)
+    {
+        for (int j = 0;j < n; j++)
+        {
+            if (i > j)
+            {
+                scanf("%d", &num);
+                if (num)
+                {
+                    return 0;
+                }
+            }
+            else
+            {
+                scanf("%d", &num);
+            }
+        }
+    }
+    return 1;
+}
+int main()
+{
+    if (result())
+    {
+        printf("YES\n");
+    }
+    else
+    {
+        printf("NO\n");
+    }
+    return 0;
+
+}
