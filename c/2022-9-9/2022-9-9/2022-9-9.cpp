1d1367af600be1767eab7bfe8781515a4b1c417f
qq8351176 <1045882688@qq.com>
2022-09-09 19:40:29 +0800

    c

---
+++ c/2022-9-9/2022-9-9/2022-9-9.cpp
@@ -0,0 +1,13 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+#include<stdio.h>
+#include <string.h>
+
+int main()
+{
+	char arr[10] = "xxxxxxxxx";
+	const char* p = "abcdef";
+	char arr2[] = { 'b','i','t' };
+	strcpy(arr, p);
+	printf("%s", p);
+	return 0;
+}
