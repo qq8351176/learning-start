176ed9562b854925b51077062518f898377c5cd1
qq8351176 <1045882688@qq.com>
2022-09-16 19:24:01 +0800

    c-learn

--- c/2022-9-9/2022-9-9/2022-9-9.cpp
+++ c/2022-9-9/2022-9-9/2022-9-9.cpp
@@ -1,13 +1,65 @@
 #define _CRT_SECURE_NO_WARNINGS 1
 #include<stdio.h>
 #include <string.h>
-my_strcpy
+#include<assert.h>
+//char* my_strcpy(char* dest, const char* src)
+//{
+//	assert(dest);
+//	assert(src);
+//}
+//int main()
+//{
+//	char arr[10] = "xxxxxxxxx";
+//	const char* p = "abcdef";
+//	char arr2[] = { 'b','i','t' };
+//	strcpy(arr, p);
+//	printf("%s", p);
+//	return 0;
+//}
+//char* my_strcat(char* dest, const char* src)
+//{
+//	assert(dest);
+//	assert(src);
+//	char* cur= dest;
+//	while (*cur != '\0')
+//	{
+//		cur++;
+//	}
+//	while (*cur++ = *src++)
+//	{
+//		;
+//	}
+//	return dest;
+//}
+//int main()
+//{
+//	char arr[20] = "hello xxxx\0xxxxxx";
+//	char arr2[] = "world";
+//	
+//	printf("%s", my_strcat(arr, arr));//这样写就报错 因为一直在覆盖
+//
+//	return 0;
+//}
+int my_strcmp(const char* s1, const char* s2)
+{
+	assert(s1);
+	assert(s2);
+	while (*s1 == *s2)
+	{
+		if (*s1 == 0)
+			return 0;
+		s1++;
+		s2++;
+	}
+	if (*s1 > *s2)
+		return 1;
+	else
+		return -1;
+}
 int main()
 {
-	char arr[10] = "xxxxxxxxx";
-	const char* p = "abcdef";
-	char arr2[] = { 'b','i','t' };
-	strcpy(arr, p);
-	printf("%s", p);
-	return 0;
+	char arr1[] = "abcd";
+	char arr2[] = "abcf";
+	int ret = my_strcmp(arr1, arr2);
+	printf("%d", ret);
 }
---
+++ c/9-12/9-12/9-12.cpp
@@ -0,0 +1,52 @@
+﻿#define _CRT_SECURE_NO_WARNINGS 1
+#include <stdio.h>
+#include <string.h>
+#include <errno.h>
+int main()
+{
+	//
+	//strerror 将错误码转换为错误信息
+	//printf("%s\n", strerror(0));
+	//printf("%s\n", strerror(1));
+	//printf("%s\n", strerror(2));
+	//printf("%s\n", strerror(3));
+	//printf("%s\n", strerror(4));
+	//printf("%s\n", strerror(5));
+	//c语言库函数报错的时候的错误码
+	//
+
+	//错误码记录到错误码变量中
+	//<errno.h>
+	FILE* pf =  fopen("test.txt", "r");
+	if(pf==NULL)
+	{ 
+		perror("fopen");
+		return 1;
+	}
+
+	fclose(pf);
+	pf = NULL;
+	return 0;
+}
+//int main()
+//{
+//	char arr[] = "kwd@qq.com";
+//	char buf[200] = { 0 };
+//	strcpy(buf, arr);
+//
+//	char* str = NULL;
+//	char p[] = "@.";
+//	for (str = strtok(buf,p) ;str!=NULL;str=strtok(NULL,p))
+//	{
+//		printf("%s\n",str);
+//	}
+//	//const char* p = "@.";
+//	//char* str = strtok(buf, p);
+//	//printf("%s\n", str);
+//
+//	//str = strtok(NULL, p);
+//	//printf("%s\n", str);
+//
+//	//str = strtok(NULL, p);
+//	//printf("%s\n", str);
+//}
